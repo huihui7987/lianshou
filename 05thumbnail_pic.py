1e14fc5cfc392dae9fed33f58f6e11000797f3d9
huihui7987 <huihui7987@gmail.com>
2016-08-14 20:54:55 +0800

    0005

---
+++ 05thumbnail_pic.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'第 0005 题：你有一个目录，装了很多照片，把它们的尺寸变成都不大于 iPhone5 分辨率(1136x640)的大小。'
+
+import os
+import glob
+from  PIL import Image
+
+def thumbnail_pic(path):
+    a = glob.glob(r'*.jpg')
+
+    for x in a:
+        name = os.path.join(path,x)
+        im = Image.open(name)
+        im.thumbnail((1136,640))
+        print(im.format,im.size,im.mode)
+        im.save(name,'JPEG')
+    print('Done')
+
+if __name__ == '__main__':
+    path = '/home/huihui7987/桌面'
+    thumbnail_pic(path)

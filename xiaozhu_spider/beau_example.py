4131d953dead692c5d25d3b37c481cc5ec379831
huihui7987 <huihui7987@gmail.com>
2016-08-29 23:45:55 +0800

    spider with python ganji---抓取物品链接，多线程还没有实现

--- xiaozhu_spider/beau_example.py
+++ xiaozhu_spider/beau_example.py
@@ -1,12 +1,96 @@
-dd = [{'huxing': '房屋户型：2室1厅1卫1厨1阳台', 'beds_num': '共3张', 'address': '北京市东城区广渠门北里55号金桥国际酒店公寓', 'rent_type': '整套出租', 'price': '668', 'img': 'http://image.xiaozhustatic1.com/00,800,533/7,0,28,8368,1800,1200,916ca9e8.jpg', 'user_num': '宜住4人', 'title': '市中心两居天安门北京站天坛国贸临铁7号', 'housesize': '房屋面积：72平米'},
-      {'huxing': '房屋户型：2室1厅1卫1厨1阳台', 'beds_num': '共3张', 'address': '北京市东城区广渠门北里55号金桥国际酒店公寓', 'rent_type': '整套出租', 'price': '668', 'img': 'http://image.xiaozhustatic1.com/00,800,533/7,0,28,8368,1800,1200,916ca9e8.jpg', 'user_num': '宜住4人', 'title': '市中心两居天安门北京站天坛国贸临铁7号', 'housesize': '房屋面积：72平米'}
-]
+#-*-coding:utf-8-*-
 
-with open ('ttt.txt','w') as f:
-    for i in dd:
-        for key in i:
-            f.write(key+':')
-            for v in i[key]:
-                f.write(v)
-            f.write('\n')
-        f.write('\n')
+import requests,time
+from bs4 import BeautifulSoup
+from multiprocessing import Pool
+
+url = 'http://bj.ganji.com/wu/'
+
+def get_source(baseurl):
+    data = requests.get(baseurl)
+    data.encoding = 'utf-8'
+    soup = BeautifulSoup(data.text,'lxml')
+    return soup
+
+def get_channel_pages_urllist(startPage,endPage,url):#获得分类指定页数页面链接
+    channel_page_urllist = []
+    soup = get_source(url)
+
+    catalog = soup.select('dl.fenlei dt a')
+    for i in catalog:
+        channel_url = 'http://bj.ganji.com'+i['href']
+        for x in range(startPage,endPage+1):
+            page_url = channel_url+'o{}'.format(x)
+            #print(page_url)
+
+        #print(channel_url)
+            channel_page_urllist.append(channel_url)
+    return channel_page_urllist
+
+channel_page_urllist = get_channel_pages_urllist(1,2,url)
+
+
+def get_goods_url(pageurl):
+    goods_urllist = []
+    soup = get_source(pageurl)
+    if soup.find('ul', 'pageLink clearfix'):
+        goods_urls = soup.select('li.js-item a[href^="http://bj.ganji.com/"]')
+        for goods_url in goods_urls:
+            goods_url = goods_url.get('href')
+            print(goods_url)
+            goods_urllist.append(goods_url)
+    #else:
+    #    break
+    return goods_urllist
+
+###上面可以实现抓取所有物品的链接
+'''
+
+if __name__ == '__main__':
+    pool = Pool(processes=4)
+    pool.map(get_goods_url(channel_page_urllist),channel_page_urllist)
+    pool.close()
+    pool.join()
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+get_channel_pages_urllist(1,10,url)
+
+
